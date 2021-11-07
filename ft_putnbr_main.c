51e8cfc3238a93065c5b989d482a432d94d988fd
Bengisu Kandemir <bkandemi@student.hive.fi>
2021-11-07 09:41:20 +0200

    test added for ft_putnbr

---
+++ ft_putnbr_main.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+int main(void)
+{
+	ft_putnbr(0);
+	ft_putchar('\n');
+	ft_putnbr(1);
+	ft_putchar('\n');
+	ft_putnbr(12);
+	ft_putchar('\n');
+	ft_putnbr(123);
+	ft_putchar('\n');
+	ft_putnbr(1234);
+	ft_putchar('\n');
+	ft_putnbr(2147483647);
+	ft_putchar('\n');
+	ft_putnbr(-1);
+	ft_putchar('\n');
+	ft_putnbr(-2147483647);
+	ft_putchar('\n');
+	ft_putnbr(-2147483648);
+	ft_putchar('\n');
+
+
+	return (0);
+
+}
