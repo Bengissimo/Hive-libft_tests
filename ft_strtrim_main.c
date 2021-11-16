dc724dad8fc32dcb3f4791eb0d56f42ad3eb2007
Bengisu Kandemir <bkandemi@c3r5p3.hive.fi>
2021-11-16 15:34:11 +0200

    ft_strtrim_main.c updated

--- ft_strtrim_main.c
+++ ft_strtrim_main.c
@@ -12,8 +12,5 @@ int main(void)
 	printf("(%s) -> (%s)\n", "		Hello world", ft_strtrim("		Hello world"));
 	printf("(%s) -> (%s)\n", "		Hello world		", ft_strtrim("		Hello world		"));
 	printf("(%s) -> (%s)\n", "\nHello world\n", ft_strtrim("\nHello world\n"));
-
-
-
 	return (0);
 }
