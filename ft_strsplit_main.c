84dd4a85eb4723c9da1c71d3d905c4cb5b9ea76a
Bengisu Kandemir <bkandemi@student.hive.fi>
2021-12-06 22:45:25 +0200

    libft-unit-test crashed condition added to the test

--- ft_strsplit_main.c
+++ ft_strsplit_main.c
@@ -6,7 +6,7 @@
 /*   By: bkandemi <bkandemi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 14:06:57 by bkandemi          #+#    #+#             */
-/*   Updated: 2021/11/23 21:07:48 by bkandemi         ###   ########.fr       */
+/*   Updated: 2021/12/06 22:40:57 by bkandemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,14 @@ int main(void)
 	}
 	printf("[OK]\n");
 	free_array(array, 5);
+	
+	//testing alelievr libft-unit-test crashed one
+	char **array2 = ft_strsplit("      split       this for   me  !       ", ' ');
+	char	**expected = ((char*[6]){"split", "this", "for", "me", "!", NULL});
+	for (int i = 0; i < 6; i++)
+	{
+		printf("%s\n", array2[i]);
+		printf("ft_strequ %d\n", ft_strequ(array2[i], expected[i]));
+	}
 	return (0);
 }
